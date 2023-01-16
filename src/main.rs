d534b126a7e7a301c3f9a68df13d02c1a86f4b23
Sam Lockart <sam.lockart@zendesk.com>
2023-01-16 17:36:01 +1100

    Start pulling token from web site instead of having to supply

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use futures::{executor::block_on, stream::StreamExt};
-use paho_mqtt as mqtt;
-use std::{env, process, time::Duration, error::Error};
-use serde::Deserialize;
-use serde_json::Value;
 use html_parser::{Dom, Result};
+use paho_mqtt as mqtt;
+use select::document::{self, Document};
+use select::predicate::{Any, Attr, Class, Name, Predicate, Text};
+use serde::Deserialize;
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::{env, error::Error, process, time::Duration};
 
 // Stat seems to mean status, tele I have no idea and cmnd is command
 // Once a topic is subscribed to, you need to publish to the relevant "cmnd"
@@ -49,35 +52,107 @@ struct Config {
     wort_unit: String,
 }
 
-fn login(username: String, password: String) -> Result<()> {
+struct SpeidelClient {
+    username: String,
+    password: String,
+    machines: HashMap<String, Machine>,
+}
 
-    let client = reqwest::blocking::ClientBuilder::new()
-        .cookie_store(true)
-        .gzip(true)
-        .build()
-        .unwrap();
+struct Machine {
+    api_token: String,
+    api_hostname: String,
+}
 
-    let params = [("identity", username),("password", password), ("submit", "".to_string())];
-    client.post("https://www.myspeidel.com/auth/login")
-        .form(&params)
-        .send()
-        .unwrap();
+impl SpeidelClient {
+    fn new(username: String, password: String) -> SpeidelClient {
+        SpeidelClient {
+            username,
+            password,
+            machines: HashMap::new(),
+        }
+    }
 
-    println!("{:?}", &params);
+    fn add_machine(&mut self, machine_id: String, api_token: String, api_hostname: String) {
+        self.machines.insert(
+            machine_id,
+            Machine {
+                api_token,
+                api_hostname,
+            },
+        );
+    }
 
-    let index = client.get("https://www.myspeidel.com/myspeidel/index")
-	.send()
-	.unwrap()
-	.text()
-	.unwrap();
+    fn login(&mut self) -> Result<()> {
+        let client = reqwest::blocking::ClientBuilder::new()
+            .cookie_store(true)
+            .gzip(true)
+            .build()
+            .unwrap();
 
+        let params = [("identity", &self.username), ("password", &self.password)];
 
-    let json = Dom::parse(index.as_str())?.to_json_pretty()?;
+        client
+            .post("https://www.myspeidel.com/auth/login")
+            .form(&params)
+            .send()
+            .unwrap();
 
-    println!("{}", json);
+        let index = client
+            .get("https://www.myspeidel.com/myspeidel/index")
+            .send()
+            .unwrap();
 
+        match index.url().path() {
+            "/auth/login" => panic!("Username or password is incorrect"),
+            "/myspeidel/index" => {
+                let page = index.text().unwrap();
+                let doc = Document::from(page.as_str());
 
-    Ok(())
+                for machine in doc.find(Class("device-list").descendant(Class("teaser-box-item"))) {
+                    let machine_id = machine.attr("data-machine-id").unwrap();
+
+                    let resp = client
+                        .get(format!(
+                            "https://www.myspeidel.com/braumeister/control/{}",
+                            machine_id
+                        ))
+                        .send()
+                        .unwrap()
+                        .text()
+                        .unwrap();
+
+                    let doc = Document::from(resp.as_str());
+                    let bmv2control_data: Vec<Value> = doc
+                        .find(Name("script").descendant(Any))
+                        .filter(|script| script.text().contains("bmv2controlData"))
+                        .map(|script| {
+                            serde_json::from_str(
+                                script
+                                    .text()
+                                    .trim()
+                                    .strip_prefix("var bmv2controlData=")
+                                    .unwrap()
+                                    .strip_suffix(';')
+                                    .unwrap(),
+                            )
+                            .unwrap()
+                        })
+                        .collect();
+
+                    for config in bmv2control_data.iter() {
+                        let machine_id = config["machineId"].to_string();
+                        let api_token = config["apiAuthToken"].to_string();
+                        let api_hostname = config["apiHostName"].to_string();
+                        self.add_machine(machine_id, api_token, api_hostname)
+                    }
+                }
+            }
+
+            _ => panic!("Unable to login for an unknown reason: {:?}", index),
+        }
+
+        Ok(())
+    }
 }
 
 fn main() {
@@ -88,46 +163,48 @@ fn main() {
         .nth(1)
         .unwrap_or_else(|| "wss://api.cloud.myspeidel.com:443".to_string());
 
-    let token = env::var("SPEIDEL_TOKEN").unwrap_or_else(|e| {
-	println!("The SPEIDEL_TOKEN environment variable must be provided: {:?}", e);
-	process::exit(1);
-    });
-
     let username = env::var("SPEIDEL_USERNAME").unwrap_or_else(|e| {
-	println!("The SPEIDEL_USERNAME environment variable must be provided: {:?}", e);
-	process::exit(1);
+        println!(
+            "The SPEIDEL_USERNAME environment variable must be provided: {:?}",
+            e
+        );
+        process::exit(1);
     });
 
     let password = env::var("SPEIDEL_PASSWORD").unwrap_or_else(|e| {
-	println!("The SPEIDEL_PASSWORD environment variable must be provided: {:?}", e);
-	process::exit(1);
+        println!(
+            "The SPEIDEL_PASSWORD environment variable must be provided: {:?}",
+            e
+        );
+        process::exit(1);
     });
 
-    login(username, password);
-	
+    let mut s = SpeidelClient::new(username, password);
+    s.login();
 
-    let machine_id = env::var("MACHINE_ID").unwrap_or_else(|e| {
-	println!("The MACHINE_ID environment variable must be provided: {:?}", e);
-	process::exit(1);
-    });
+    let mut machine_id = String::new();
+    let mut api_token = String::new();
+    for m in &s.machines.into_keys().into_iter() {
+        machine_id = m;
+        api_token = s.machines.get(&m).unwrap().api_token;
+    }
 
     let mut topics = vec![];
     let mut commands = vec![];
 
     for &prefix in PREFIX {
-	for &topic in TOPICS {
-	    if prefix == "cmnd" {
-		commands.push(format!("{}/{}/{}", prefix, machine_id, topic));
-	    } else {
-		topics.push(format!("{}/{}/{}", prefix, machine_id, topic));
-	    }
-	}
+        for &topic in TOPICS {
+            if prefix == "cmnd" {
+                commands.push(format!("{}/{}/{}", prefix, machine_id, topic));
+            } else {
+                topics.push(format!("{}/{}/{}", prefix, machine_id, topic));
+            }
+        }
     }
 
     let qos = vec![0_i32; topics.len()];
 
     print!("{:?}", topics);
-
 
     // Create the client. Use an ID for a persistent session.
     // A real system should try harder to use a unique ID.
@@ -149,12 +226,11 @@ fn main() {
         // Define the set of options for the connection
         let conn_opts = mqtt::ConnectOptionsBuilder::new()
             .keep_alive_interval(Duration::from_secs(30))
-	    .user_name(token)
-	    .password("")
-	    .ssl_options(mqtt::SslOptionsBuilder::new().finalize())
+            .user_name(api_token)
+            .password("")
+            .ssl_options(mqtt::SslOptionsBuilder::new().finalize())
             .clean_session(false)
             .finalize();
-
 
         // Make the connection to the broker
         println!("Connecting to the MQTT server...");
@@ -163,11 +239,11 @@ fn main() {
         println!("Subscribing to topics: {:?}", TOPICS);
         cli.subscribe_many(&topics, qos.as_slice()).await?;
 
-	println!("Publishing request to receieve updates ({:?})...", commands);
-	for command in commands {
-	    let msg = mqtt::Message::new(command, "", mqtt::QOS_1);
-	    cli.publish(msg).await?;
-	}
+        println!("Publishing request to receieve updates ({:?})...", commands);
+        for command in commands {
+            let msg = mqtt::Message::new(command, "", mqtt::QOS_1);
+            cli.publish(msg).await?;
+        }
 
         // Just loop on incoming messages.
         println!("Waiting for messages...");
@@ -178,30 +254,31 @@ fn main() {
 
         while let Some(msg_opt) = strm.next().await {
             if let Some(msg) = msg_opt {
-		let p: Value = match serde_json::from_str(msg.payload_str().into_owned().as_str()) {
-		    Ok(v) => v,
-		    Err(e) => panic!("Recieved empty message, token may have expired: {e}"),
-		};
-		match p["topic"].as_str().unwrap() {
-		    "braumeister/machineinfo" => {
-			let m: MachineInfo = serde_json::from_str(p["body"].to_string().as_str()).unwrap();
-			print!("{:?}", m);
-		    },
-		    "braumeister/status" => {
-			let m: Status = serde_json::from_str(p["body"].to_string().as_str()).unwrap();
-			print!("{:?}", m);
-		    },
-		    "braumeister/config" => {
-			let m: Config = serde_json::from_str(p["body"].to_string().as_str()).unwrap();
-			print!("{:?}", m);
-		    },
-		    unknown => {
-			println!("received unknown topic: {}", unknown);
-			
-		    }
-		}
-            }
-            else {
+                let p: Value = match serde_json::from_str(msg.payload_str().into_owned().as_str()) {
+                    Ok(v) => v,
+                    Err(e) => panic!("Recieved empty message, token may have expired: {e}"),
+                };
+                match p["topic"].as_str().unwrap() {
+                    "braumeister/machineinfo" => {
+                        let m: MachineInfo =
+                            serde_json::from_str(p["body"].to_string().as_str()).unwrap();
+                        print!("{:?}", m);
+                    }
+                    "braumeister/status" => {
+                        let m: Status =
+                            serde_json::from_str(p["body"].to_string().as_str()).unwrap();
+                        print!("{:?}", m);
+                    }
+                    "braumeister/config" => {
+                        let m: Config =
+                            serde_json::from_str(p["body"].to_string().as_str()).unwrap();
+                        print!("{:?}", m);
+                    }
+                    unknown => {
+                        println!("received unknown topic: {}", unknown);
+                    }
+                }
+            } else {
                 // A "None" means we were disconnected. Try to reconnect...
                 println!("Lost connection. Attempting reconnect.");
                 while let Err(err) = cli.reconnect().await {
